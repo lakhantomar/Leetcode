6fe3428d44ac33ceddf1ef99d06ce0cf78cb069d
Lakhan Tomar <lakhantomar7866@gmail.com>
2025-04-22 23:20:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int fib(int n) {
+
+        if (n <= 1) {
+            return n;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0]
+
+        }
+
+        if(n<=1)
+
+        else if (map.containsKey)(n) {
+            return map.get(n);
+
+        }
+        int a = fib(n - 1, map);
+        int b = fib
+  
+   
+  
+
+   
+        
+   
